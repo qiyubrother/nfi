f1fd236f9f820cb2f0193c2351407ca0501312c3
13240137763@163.com <qiyubrother>
2020-03-09 17:02:42 +0800

    .

--- nfi/Program.cs
+++ nfi/Program.cs
@@ -14,23 +14,23 @@ namespace nfi
         static void Main(string[] args)
         {
 #if false
-            if (args.Length == 0)
-            {
-                Console.Write("");
-                return;
-            }
-            var fi = new FileInfo(args[0]);
-            if (!fi.Exists)
-            {
-                Console.Write("");
-                return;
-            }
+            //if (args.Length == 0)
+            //{
+            //    Console.Write("");
+            //    return;
+            //}
+            //var fi = new FileInfo(args[0]);
+            //if (!fi.Exists)
+            //{
+            //    Console.Write("");
+            //    return;
+            //}
             var f = @"D:\SVNRoot\src\Client\win\teacherMoveOffice\zntbkt\bin\Debug\zntbkt.exe";
 #else
             var f = args[0];
 #endif
             var ver = Assembly.LoadFile(f).GetName().Version;
-            var v = $"V{ver.Major}.{ver.MajorRevision}.{ver.MinorRevision}";
+            var v = $"V{ver.Major}.{ver.MajorRevision}.{ver.Build}";
             Console.Write(v);
         }
     }
